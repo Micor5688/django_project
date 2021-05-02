e1b5a9ff72bc8c04e6247c502035c039f48409c2
micor6588 <micor5688@163.com>
2021-05-02 17:25:10 +0800

    初探视图View

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#导入 HttpResponse模块
+from django.http import HttpResponse
 
 # Create your views here.
+# 视图函数
+def article_list(request):
+    return HttpResponse("Hello World!")
